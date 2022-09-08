89cc38cb80d0c4189ca4be8441c835dcf91b4224
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-09-08 22:58:12 +0300

    feat: adding position entity

---
+++ src/main/java/com/example/enterprisefinalexam/model/Position.java
@@ -0,0 +1,31 @@
+package com.example.enterprisefinalexam.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "position")
+public class Position {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
