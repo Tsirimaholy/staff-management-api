fdfc7390975e58ad2eb4b78af3ecc6d1d3fbe733
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-09-08 07:20:45 +0300

    chore: initialised project

---
+++ src/main/java/com/example/enterprisefinalexam/EnterpriseFinalExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.enterprisefinalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseFinalExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseFinalExamApplication.class, args);
+    }
+
+}
